ace7eeb910a549c550bb37f786985a5aa078be85
samuel hernandez <samuelop99op@gmail.com>
2023-11-08 17:00:00 -0700

    funciona el formulario

--- src/components/DeviceForm.jsx
+++ src/components/DeviceForm.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, Button, Form, Input, Select, Upload, message } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Modal, Form, Input, Select, message, Button } from "antd";
+import axios from "axios";
+import FormData from "form-data";
 
-const DeviceForm = () => {
+const DeviceForm = ({ onClose }) => {
   const [form] = Form.useForm();
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(true);
   const [owners, setOwners] = useState([]);
-  const [formLayout, setFormLayout] = useState('vertical');
+  const [formLayout, setFormLayout] = useState("vertical");
 
   useEffect(() => {
     axios
-      .get('http://143.198.148.125/api/catalog/owners')
+      .get("http://143.198.148.125/api/catalog/owners")
       .then((response) => {
         setOwners(response.data.data);
       })
       .catch((error) => {
-        console.error('Error al obtener la lista de propietarios', error);
+        console.error("Error al obtener la lista de propietarios", error);
       });
   }, []);
 
@@ -26,112 +26,184 @@ const DeviceForm = () => {
 
   const handleCancel = () => {
     setIsModalVisible(false);
+    onClose();
   };
 
   const sendDeviceData = (values) => {
-    const selectedOwner = owners.find((owner) => owner.name === values.owner_name);
+    const selectedOwner = owners.find(
+      (owner) => owner.name === values.owner_name
+    );
     if (selectedOwner) {
       const ownerId = selectedOwner.id;
-      console.log('ID del propietario seleccionado:', ownerId);
+      console.log("ID del propietario seleccionado:", ownerId);
     }
 
-    const deviceData = {
-      state: values.state,
-      device_type: values.device_type,
-      brand: values.brand,
-      damage: values.damage,
-      accesories: values.accesories,
-      technican: values.technican,
-      owner_id: values.owner_id,
-      img: values.img,
-    };
+    const formData = new FormData();
+    formData.append("state", values.state);
+    formData.append("device_type", values.device_type);
+    formData.append("brand", values.brand);
+    formData.append("damage", values.damage);
+    formData.append("accesories", values.accesories);
+    formData.append("technican", values.technican);
+    formData.append("owner_id", values.owner_id);
+    formData.append("img", values.img[0]);
 
-    return deviceData;
+    return formData;
   };
 
-  const onFinish = (values) => {
-    const deviceData = sendDeviceData(values);
+  const onFinish = async (values) => {
+    const formData = sendDeviceData(values);
 
-    axios
-      .post('http://143.198.148.125/api/devices', deviceData)
-      .then((response) => {
-        message.success('Dispositivo agregado exitosamente');
-        console.log('Respuesta del servidor:', response.data);
-      })
-      .catch((error) => {
-        message.error('Error al agregar el dispositivo');
-        console.error('Error al agregar el dispositivo', error);
-      });
+    try {
+      const response = await axios.post(
+        "http://143.198.148.125/api/devices",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      message.success("Dispositivo agregado exitosamente");
+      console.log("Respuesta del servidor:", response.data);
+    } catch (error) {
+      message.error("Error al agregar el dispositivo");
+      console.error("Error al agregar el dispositivo", error);
+    }
 
     setIsModalVisible(false);
+    onClose();
   };
 
   return (
-    <Form
-      form={form}
-      layout={formLayout}
-      style={{ maxWidth: '600px' }}
-      onFinish={onFinish}
-      initialValues={{ layout: formLayout }}
-      onValuesChange={({ layout }) => {
-        setFormLayout(layout);
-      }}
+    <Modal
+      title="Agregar Dispositivo"
+      visible={isModalVisible}
+      onCancel={handleCancel}
+      footer={null}
     >
-      <Button type="primary" onClick={showModal}>
-        Agregar Dispositivo
-      </Button>
-      <Modal title="Agregar Dispositivo" visible={isModalVisible} onCancel={handleCancel} footer={null}>
-        <Form.Item label="Estado" name="state" rules={[{ required: true, message: 'Por favor seleccione el estado' }]}>
-          <Select>
-            <Select.Option value="Recibido">Recibido</Select.Option>
-            <Select.Option value="En Proceso">En Proceso</Select.Option>
-            <Select.Option value="Terminado">Terminado</Select.Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="Tipo de Dispositivo" name="device_type" rules={[{ required: true, message: 'Por favor ingrese el tipo de dispositivo' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Marca" name="brand" rules={[{ required: true, message: 'Por favor ingrese la marca' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Daño" name="damage">
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Accesorios" name="accesories">
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Técnico" name="technican">
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Propietario" name="owner_name">
-          <Select>
-            {owners.map((owner) => (
-              <Select.Option key={owner.id} value={owner.id}>
-                {owner.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="Imagen" name="img">
-  <Upload
-    beforeUpload={() => false} // Prevent default behavior to open the file dialog
-  >
-    <Button icon={<UploadOutlined />}>Seleccionar Archivo</Button>
-  </Upload>
-</Form.Item>
-
-        <Button type="primary" htmlType="submit" onClick={() => onFinish(form.getFieldsValue())}>
-          Guardar
+      <Form
+        form={form}
+        layout={formLayout}
+        style={{ maxWidth: "600px" }}
+        onFinish={onFinish}
+        initialValues={{ layout: formLayout }}
+        onValuesChange={({ layout }) => {
+          setFormLayout(layout);
+        }}
+      >
+        <Button type="primary" onClick={showModal}>
+          Agregar Dispositivo
         </Button>
-      </Modal>
-    </Form>
+        <Modal
+          title="Agregar Dispositivo"
+          visible={isModalVisible}
+          onCancel={handleCancel}
+          footer={null}
+        >
+          <Form.Item
+            label="Estado"
+            name="state"
+            rules={[
+              { required: true, message: "Por favor seleccione el estado" },
+            ]}
+          >
+            <Select>
+              <Select.Option value="Recibido">Recibido</Select.Option>
+              <Select.Option value="En Proceso">En Proceso</Select.Option>
+              <Select.Option value="Terminado">Terminado</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Tipo de Dispositivo"
+            name="device_type"
+            rules={[
+              {
+                required: true,
+                message: "Por favor ingrese el tipo de dispositivo",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Marca"
+            name="brand"
+            rules={[{ required: true, message: "Por favor ingrese la marca" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Daño"
+            name="damage"
+            rules={[{ required: true, message: "Por favor ingrese el daño" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Accesorios"
+            name="accesories"
+            rules={[
+              { required: true, message: "Por favor ingrese los accesorios" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Técnico"
+            name="technican"
+            rules={[
+              { required: true, message: "Por favor ingrese el tecnico" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Propietario"
+            name="owner_id"
+            rules={[{ required: true, message: "Por favor ingrese el dueño" }]}
+          >
+            <Select>
+              {owners.map((owner) => (
+                <Select.Option key={owner.id} value={owner.id}>
+                  {owner.name}-{owner.phone_number}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Imagen"
+            name="img"
+            valuePropName="fileList"
+            rules={[{ required: true, message: "Por favor ingrese la marca" }]}
+          >
+            <input
+              type="file"
+              accept=".jpg, .png, .jpeg"
+              onChange={(e) =>
+                form.setFieldsValue({ img: [e.target.files[0]] })
+              }
+            />
+          </Form.Item>
+
+          <Button
+            type="primary"
+            htmlType="submit"
+            onClick={() => onFinish(form.getFieldsValue())}
+          >
+            Guardar
+          </Button>
+        </Modal>
+      </Form>
+    </Modal>
   );
 };
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,13 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Button, Popconfirm, Space, Menu, Dropdown, Input ,Tooltip} from 'antd';
-import { PlusCircleFilled , PrinterFilled , DeleteFilled , EditFilled } from '@ant-design/icons';
-
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  Button,
+  Popconfirm,
+  Space,
+  Menu,
+  Dropdown,
+  Input,
+  Tooltip,
+  Modal,
+} from "antd";
+import {
+  PlusCircleFilled,
+  PrinterFilled,
+  DeleteFilled,
+  EditFilled,
+} from "@ant-design/icons";
+import DeviceForm from "./DeviceForm";
+import axios from "axios";
 
 const DataTable = ({ onDelete, onEdit }) => {
   const { Search } = Input;
   const [data, setData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false); // Agrega el estado para el modal
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const openForm = () => {
+    setIsFormVisible(true);
+  };
+
+  const closeForm = () => {
+    setIsFormVisible(false);
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const [pagination, setPagination] = useState({
     current: 1,
@@ -20,7 +53,7 @@ const DataTable = ({ onDelete, onEdit }) => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://143.198.148.125/api/devices', {
+      const response = await axios.get("http://143.198.148.125/api/devices", {
         params: {
           page: pagination.current,
           per_page: pagination.pageSize,
@@ -40,7 +73,7 @@ const DataTable = ({ onDelete, onEdit }) => {
 
       */
     } catch (error) {
-      console.error('Error al obtener los datos de dispositivos:', error);
+      console.error("Error al obtener los datos de dispositivos:", error);
     }
   };
 
@@ -54,57 +87,69 @@ const DataTable = ({ onDelete, onEdit }) => {
 
   const columns = [
     {
-      title: 'Cliente',
-      dataIndex: 'owner.name',
-      key: 'name',
-    render: (text, record) => (
-      <span>{record.owner.name}</span>
-    )
+      title: "Cliente",
+      dataIndex: "owner.name",
+      key: "name",
+      render: (text, record) => <span>{record.owner.name}</span>,
     },
     {
-      title: 'Estado',
-      dataIndex: 'state',
-      key: 'state',
+      title: "Estado",
+      dataIndex: "state",
+      key: "state",
     },
     {
-      title: 'Tipo de dispositivo',
-      dataIndex: 'device_type',
-      key: 'device_type',
+      title: "Tipo de dispositivo",
+      dataIndex: "device_type",
+      key: "device_type",
     },
     {
-      title: 'Marca',
-      dataIndex: 'brand',
-      key: 'brand',
+      title: "Marca",
+      dataIndex: "brand",
+      key: "brand",
     },
     {
-      title: 'Daño',
-      dataIndex: 'damage',
-      key: 'damage',
+      title: "Daño",
+      dataIndex: "damage",
+      key: "damage",
     },
     {
-      title: 'Accesorios',
-      dataIndex: 'accesories',
-      key: 'accesories',
+      title: "Accesorios",
+      dataIndex: "accesories",
+      key: "accesories",
     },
     {
-      title: 'Fecha',
-      dataIndex: 'created_at',
-      key: 'created_at',
+      title: "Fecha",
+      dataIndex: "created_at",
+      key: "created_at",
     },
     {
-      title: 'Acciones',
-      key: 'actions',
+      title: "Acciones",
+      key: "actions",
       render: (_, record) => (
         <Space size="middle">
-          
-          <Button icon={<PrinterFilled />} onClick={() => handlePrint(record)}></Button>
+          <Button
+            icon={<PrinterFilled />}
+            onClick={() => handlePrint(record)}
+          ></Button>
 
           <Tooltip title="Agregar Actualizacion">
-            <Button style={{ background: '#96be25', borderColor: '#96be25', color: 'white' }}  icon={<PlusCircleFilled />} onClick={() => handleAddUpdate(record)}>
-              </Button></Tooltip>
-          
+            <Button
+              style={{
+                background: "#96be25",
+                borderColor: "#96be25",
+                color: "white",
+              }}
+              icon={<PlusCircleFilled />}
+              onClick={() => handleAddUpdate(record)}
+            ></Button>
+          </Tooltip>
+
           <Tooltip title="Editar">
-            <Button icon={<EditFilled />} type="primary" onClick={() => onEdit(record)}></Button>
+            <Button
+              icon={<EditFilled />}
+              type="primary"
+              onClick={() => onEdit(record)}
+            ></Button>
           </Tooltip>
 
           <Popconfirm
@@ -113,7 +158,7 @@ const DataTable = ({ onDelete, onEdit }) => {
             okText="Sí"
             cancelText="No"
           >
-            <Button icon = {<DeleteFilled />} type="primary" danger></Button>
+            <Button icon={<DeleteFilled />} type="primary" danger></Button>
           </Popconfirm>
         </Space>
       ),
@@ -123,52 +168,53 @@ const DataTable = ({ onDelete, onEdit }) => {
   const handleTableChange = (pagination) => {
     setPagination(pagination);
   };
-
+  const handleAddDevice = (record) => {
+    console.log("ID DEL DISPOSITIVO:", record.id);
+  };
   const handleSearch = (value) => {
     setSearchQuery(value);
   };
   const handlePrint = async (record) => {
     try {
-      console.log('ID DEL DISPOSITIVO:', record.id);
+      console.log("ID DEL DISPOSITIVO:", record.id);
       // Realiza una solicitud HTTP al endpoint con el ID del registro seleccionado
       const response = await axios({
-        method: 'GET',
+        method: "GET",
         url: `http://www.pcexpressbcs.com.mx/api/devices/${record.id}/ticket`,
-        responseType: 'arraybuffer', // Importante: solicitar una respuesta en formato de array de bytes (binario)
+        responseType: "arraybuffer", // Importante: solicitar una respuesta en formato de array de bytes (binario)
       });
-  
+
       // Verificar que la solicitud fue exitosa
       if (response.status === 200) {
         // Crear un Blob a partir de los datos del PDF
-        const blob = new Blob([response.data], { type: 'application/pdf' });
+        const blob = new Blob([response.data], { type: "application/pdf" });
         const url = window.URL.createObjectURL(blob);
-  
+
         // Crear un enlace de descarga
-        const a = document.createElement('a');
-        a.style.display = 'none';
+        const a = document.createElement("a");
+        a.style.display = "none";
         a.href = url;
         a.download = `ticket_${record.id}.pdf`; // Nombre del archivo para la descarga
         document.body.appendChild(a);
-  
+
         // Simular un clic en el enlace para iniciar la descarga
         a.click();
-  
+
         // Limpiar después de la descarga
         window.URL.revokeObjectURL(url);
         document.body.removeChild(a);
       } else {
-        console.error('Error al imprimir: No se pudo obtener el PDF');
+        console.error("Error al imprimir: No se pudo obtener el PDF");
       }
     } catch (error) {
-      console.error('Error al imprimir:', error);
+      console.error("Error al imprimir:", error);
     }
   };
-  
 
   const filteredData = data.filter((item) => {
     // Filtra en base a la búsqueda en todas las propiedades del objeto
     return Object.values(item).some((property) => {
-      if (typeof property === 'string') {
+      if (typeof property === "string") {
         return property.toLowerCase().includes(searchQuery.toLowerCase());
       }
       return false;
@@ -177,22 +223,27 @@ const DataTable = ({ onDelete, onEdit }) => {
 
   return (
     <div>
-      
       <Search
         placeholder="Buscar en la tabla"
         onSearch={handleSearch}
         style={{ width: 200, marginBottom: 16 }}
       />
       <Tooltip title="Agregar Dispositivo">
-      <Button type="primary" icon={<PlusCircleFilled />}  onClick={() => handlePrint(record)}></Button>
+        <Button
+          type="primary"
+          onClick={openForm}
+          icon={<PlusCircleFilled />}
+        ></Button>
       </Tooltip>
-      
+
       <Table
         dataSource={filteredData}
         columns={columns}
         pagination={pagination}
         onChange={handleTableChange}
       />
+
+      {isFormVisible && <DeviceForm onClose={closeForm} />}
     </div>
   );
 };
