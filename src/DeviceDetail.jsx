e835fcaad05556874405bb742a300f79dbdbc450
samuel hernandez <samuelop99op@gmail.com>
2023-11-13 16:28:38 -0700

    cambios a main

--- src/DeviceDetail.jsx
+++ src/DeviceDetail.jsx
@@ -1,11 +1,31 @@
-// DeviceDetails.js
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Typography, Card, Timeline, Space, Button, Modal } from "antd";
+import moment from "moment";
+
+const { Title, Text } = Typography;
+
+const cardStyle = {
+  width: "100%",
+  maxWidth: "800px",
+  margin: "0 auto",
+  padding: "20px",
+  borderRadius: "10px",
+  boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+};
+
+const textBlockStyle = {
+  background: "#f7f7f7",
+  padding: "10px",
+  marginBottom: "10px",
+  borderRadius: "5px",
+};
 
 const DeviceDetails = () => {
   const { id } = useParams();
   const [deviceData, setDeviceData] = useState(null);
+  const [imageModalVisible, setImageModalVisible] = useState(false); // Estado para controlar la visibilidad del modal
 
   useEffect(() => {
     // Realiza una solicitud GET para obtener los datos del dispositivo según el ID
@@ -23,14 +43,113 @@ const DeviceDetails = () => {
     return <p>Cargando...</p>;
   }
 
+  const openImageModal = () => {
+    setImageModalVisible(true);
+  };
+
+  const closeImageModal = () => {
+    setImageModalVisible(false);
+  };
+
   return (
-    <div>
-      <h1>Detalles del Dispositivo</h1>
-      <p>ID: {deviceData.id}</p>
-      <p>Estado: {deviceData.state}</p>
-      <p>Tipo de dispositivo: {deviceData.device_type}</p>
-      {/* Mostrar otros detalles del dispositivo según sea necesario */}
-    </div>
+    <Card style={cardStyle}>
+      <div style={textBlockStyle}>
+        <Title level={2}>Detalles del Dispositivo</Title>
+        <Space direction="vertical" size="20">
+          <div>
+            <Text strong>ID:</Text> {deviceData.id}
+          </div>
+          <div>
+            <Text strong>Estado:</Text> {deviceData.state}
+          </div>
+          <div>
+            <Text strong>Tipo de dispositivo:</Text> {deviceData.device_type}
+          </div>
+          <div>
+            <Text strong>Marca:</Text> {deviceData.brand}
+          </div>
+          <div>
+            <Text strong>Daño:</Text> {deviceData.damage}
+          </div>
+          <div>
+            <Text strong>Accesorios:</Text> {deviceData.accesories}
+          </div>
+          <div>
+            <Text strong>Técnico:</Text> {deviceData.technican}
+          </div>
+          <div>
+            <Text strong>Fecha de creación:</Text>{" "}
+            {moment(deviceData.created_at).format("DD-MM-YYYY HH:mm:ss")}
+          </div>
+          <div>
+            <Text strong>Última actualización:</Text>{" "}
+            {moment(deviceData.updated_at).format("DD-MM-YYYY HH:mm:ss")}
+          </div>
+        </Space>
+      </div>
+      <Button onClick={openImageModal}>Ver Imagen</Button>
+      <Modal
+        title="Imagen del Dispositivo"
+        visible={imageModalVisible}
+        onCancel={closeImageModal}
+        footer={null}
+      >
+        <img
+          src={deviceData.imageUrl}
+          alt="Imagen del dispositivo"
+          style={{ display: "block", margin: "0 auto", maxWidth: "100%" }}
+        />
+      </Modal>
+      <div style={textBlockStyle}>
+        <Title level={5}>Propietario</Title>
+        <Space direction="vertical" size="20">
+          <div>
+            <Text strong>ID del propietario:</Text> {deviceData.owner.id}
+          </div>
+          <div>
+            <Text strong>Nombre del propietario:</Text> {deviceData.owner.name}
+          </div>
+          <div>
+            <Text strong>Número de teléfono:</Text>{" "}
+            {deviceData.owner.phone_number}
+          </div>
+          <div>
+            <Text strong>Correo electrónico:</Text> {deviceData.owner.email}
+          </div>
+          <div>
+            <Text strong>Fecha de creación del propietario:</Text>{" "}
+            {moment(deviceData.owner.created_at).format("DD-MM-YYYY HH:mm:ss")}
+          </div>
+          <div>
+            <Text strong>Última actualización del propietario:</Text>{" "}
+            {moment(deviceData.owner.updated_at).format("DD-MM-YYYY HH:mm:ss")}
+          </div>
+        </Space>
+      </div>
+      <div style={textBlockStyle}>
+        <Title level={5}>Actualizaciones</Title>
+        <Timeline>
+          {deviceData.updates.map((update) => (
+            <Timeline.Item key={update.id}>
+              <div style={textBlockStyle}>
+                <Space direction="vertical" size="20">
+                  <div>
+                    <Text strong>Título:</Text> {update.Title}
+                  </div>
+                  <div>
+                    <Text strong>Descripción:</Text> {update.description}
+                  </div>
+                  <div>
+                    <Text strong>Fecha de Creación:</Text>{" "}
+                    {moment(update.created_at).format("DD-MM-YYYY HH:mm:ss")}
+                  </div>
+                </Space>
+              </div>
+            </Timeline.Item>
+          ))}
+        </Timeline>
+      </div>
+    </Card>
   );
 };
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Box,
   Card,
@@ -11,46 +11,62 @@ import {
   CardFooter,
   ButtonGroup,
   Button,
-} from '@chakra-ui/react';
+  Badge,
+} from "@chakra-ui/react";
 
 const handleDetailsClick = () => {
   // Llama a una función para abrir el modal aquí
   openDeviceModal(data.id); // Suponiendo que `openDeviceModal` proviene de `DeviceList`
 };
 
-
-const DeviceCard = ({ data , openTimelineModal,openDeviceModal}) => {
+const DeviceCard = ({ data, openTimelineModal, openDeviceModal }) => {
   return (
-    
-    <Card maxW='sm' m='5' mt='5'>
+    <Card maxW="sm" m="5" mt="5">
       <CardBody>
-        <Box display='flex' justifyContent='center'>
+        <Box display="flex" justifyContent="center">
           <Image
             src={data.imageUrl}
             alt={data.brand}
-            borderRadius='lg'
-            objectFit='cover'
-            height='200px'
-            width='100%' 
+            borderRadius="lg"
+            objectFit="cover"
+            height="200px"
+            width="100%"
           />
         </Box>
-        <Stack mt='5' spacing='5'>
-          <Heading size='md'>{data.brand}</Heading>
-          <Heading size='sm'>Orden: {data.id}</Heading>
-          <Text size='sm'>{data.owner.name}</Text>
+        <Stack mt="5" spacing="5">
+          <Heading size="md">{data.brand}</Heading>
+          <Heading size="sm">Orden: {data.id}</Heading>
+          <Text size="sm">{data.owner.name}</Text>
           <Text>{data.damage}</Text>
-          <Text color='blue.600' fontSize='2xl'>
-            {data.state === 'Active' ? 'Active' : 'Inactive'}
-          </Text>
+          <Badge
+            colorScheme={
+              data.state === "Recibido"
+                ? "red"
+                : data.state === "En Proceso"
+                ? "yellow"
+                : data.state === "Terminado"
+                ? "green"
+                : "gray"
+            }
+            fontSize="l"
+          >
+            {data.state}
+          </Badge>
         </Stack>
       </CardBody>
       <Divider />
       <CardFooter>
-        <ButtonGroup spacing='50'justifyContent='center' overflowX='auto'>
-        <Button variant='solid' colorScheme='blue' onClick={() => openDeviceModal(data.id)}>Ver Detalles</Button>
-          <Button variant='outline' 
-          colorScheme='yellow' 
-          onClick={() => openTimelineModal(data)}>Ver Historial</Button>
+        <ButtonGroup spacing="50" justifyContent="center" overflowX="auto">
+          <Button
+            variant="solid"
+            colorScheme="blue"
+            onClick={() => openDeviceModal(data.id)}
+          >
+            Ver Detalles
+          </Button>
+          <Button colorScheme="yellow" onClick={() => openTimelineModal(data)}>
+            Ver Historial
+          </Button>
         </ButtonGroup>
       </CardFooter>
     </Card>
