08c580c63feaf6189b7a78601d368f748ebc1cba
samuel hernandez <samuelop99op@gmail.com>
2023-11-25 10:15:09 -0700

    funcion de rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import Hola from "./holamundo.jsx";
@@ -8,21 +8,42 @@ import Index from "./index.jsx";
 import Pruebas from "./pruebas.jsx";
 import DeviceDetail from "./DeviceDetail.jsx";
 import NoAuth from "./NoAuth.jsx";
-import { HashRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/hola",
+    element: <Hola></Hola>,
+  },
+  {
+    path: "/noauth",
+    element: <NoAuth></NoAuth>,
+  },
+  {
+    path: "#/app",
+    element: <App></App>,
+  },
+  {
+    path: "/",
+    element: <Login></Login>,
+  },
+  {
+    path: "#/adminDevices",
+    element: <Index></Index>,
+  },
+  {
+    path: "/prueba",
+    element: <Pruebas></Pruebas>,
+  },
+  {
+    path: "#/devices/:id",
+    element: <DeviceDetail />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
-    <HashRouter>
-      <Routes>
-        <Route path="/hola" component={Hola} />
-        <Route path="/noauth" component={NoAuth} />
-        <Route path="/app" component={App} />
-        <Route path="/" exact component={Login} />
-        <Route path="/adminDevices" component={Index} />
-        <Route path="/prueba" component={Pruebas} />
-        <Route path="/devices/:id" component={DeviceDetail} />
-      </Routes>
-    </HashRouter>
-  </ChakraProvider>,
-  document.getElementById("root")
+    <RouterProvider router={router} />
+  </ChakraProvider>
 );
